eacecef0b3897afa12da5c213833229d37ccceb8
Cristina Carceller Segarra <cristinacarceller95@gmail.com>
2024-04-17 17:12:10 +0100

    NEW: second test

--- __tests__/test-file.js
+++ __tests__/test-file.js
@@ -3,17 +3,34 @@ import { addItem } from "../index.js";
 
 //? User should be able to add items to the basket
 
-// Arrange
-const item = "Bagel";
-const basket = [];
-const expected = ["Bagel"].toString;
+// // Arrange
+// const item = "Bagel";
+// const basket = [];
+// const expected = ["Bagel"].toString;
+// let actual, result;
+
+// // Act
+// actual = addItem(basket, item);
+
+// // Assert
+// result = assertEquals(actual, expected);
+
+// // Result
+// console.log(`Test Result: ${result}`);
+
+//? User should be able to remove item from basket
+
+//Arrange
+const item = "bagel";
+const basket = ["bagel"];
+const expected = [].toString;
 let actual, result;
 
-// Act
-actual = addItem(basket, item);
+//Act
+actual = removeItem(basket, item);
 
-// Assert
+//Assert
 result = assertEquals(actual, expected);
 
-// Result
-console.log(`Test Result: ${result}`);
+//Result
+console.log(result);
